a52207fc80e87540cbb546d322a7e680fcdc3d9e
Brian Van Klaveren <bvan@slac.stanford.edu>
2017-07-03 10:10:42 -0700

    PEP8 fixes and dead code removal

--- python/lsst/dax/imgserv/imageStitch.py
+++ python/lsst/dax/imgserv/imageStitch.py
@@ -28,88 +28,86 @@ Prototype image stitching code.
 
 import logging as log
 
-import lsst.afw
-import lsst.afw.geom as afwGeom
-import lsst.afw.image as afwImage
-import lsst.afw.math as afwMath
-import lsst.coadd.utils as coaddUtils
-import lsst.pex.config as pexConfig
+import lsst.afw.image as afw_image
+import lsst.coadd.utils as coadd_utils
+import lsst.pex.config as pex_config
 
-import matplotlib.pyplot as plt
 
-class CoaddConfig(pexConfig.Config):
-    saveDebugImages = pexConfig.Field(
-        doc = "Save intermediate images?",
-        dtype = bool,
-        default = False,
+class CoaddConfig(pex_config.Config):
+    saveDebugImages = pex_config.Field(
+        doc="Save intermediate images?",
+        dtype=bool,
+        default=False,
     )
-    bboxMin = pexConfig.ListField(
-        doc = "Lower left corner of bounding box used to subframe to all input images",
-        dtype = int,
-        default = (0, 0),
-        length = 2,
+    bboxMin = pex_config.ListField(
+        doc="Lower left corner of bounding box used to subframe to all input images",
+        dtype=int,
+        default=(0, 0),
+        length=2,
     )
-    bboxSize = pexConfig.ListField(
-        doc = "Size of bounding box used to subframe all input images; 0 0 for full input images",
-        dtype = int,
-        default = (0, 0),
-        length = 2,
+    bboxSize = pex_config.ListField(
+        doc="Size of bounding box used to subframe all input images; 0 0 for full input images",
+        dtype=int,
+        default=(0, 0),
+        length=2,
     )
-    coaddZeroPoint = pexConfig.Field(
-        dtype = float,
-        doc = "Photometric zero point of coadd (mag).",
-        default = 27.0,
+    coaddZeroPoint = pex_config.Field(
+        dtype=float,
+        doc="Photometric zero point of coadd (mag).",
+        default=27.0,
     )
-    coadd = pexConfig.ConfigField(dtype = coaddUtils.Coadd.ConfigClass, doc = "")
-    #warp = pexConfig.ConfigField(dtype = afwMath.Warper.ConfigClass, doc = "")
+    coadd = pex_config.ConfigField(dtype=coadd_utils.Coadd.ConfigClass, doc="")
 
-def stitchExposures(destWcs, destBBox, expoList, configCoadd, warper):
-    ''' Return an exposure matching the destWcs and destBBox that is composed of
-    pixels from the exposures in expoList. Uses coadd_utils.Coadd.
-    destWcs     - WCS object for the destination exposure.
-    destBBox    - Bounding box for the destination exposure.
-    expoList    - List of exposures to combine to form dextination exposure.
-    configCoadd - configuration for Coadd
+
+def stitch_exposures(dest_wcs, dest_bbox, expo_list, coadd_config, warper):
+    """Return an exposure matching the dest_wcs and dest_bbox that is composed of
+    pixels from the exposures in expo_list. Uses coadd_utils.Coadd.
+    dest_wcs     - WCS object for the destination exposure.
+    dest_bbox    - Bounding box for the destination exposure.
+    expo_list    - List of exposures to combine to form dextination exposure.
+    coadd_config - configuration for Coadd
     warper      - Warper to use when warping images.
     All exposures need valid WCS.
-    '''
-    coadd = coaddUtils.Coadd.fromConfig(
-        bbox = destBBox,
-        wcs = destWcs,
-        config = configCoadd)
-    for j, expo in enumerate(expoList):
-        warpedExposure = warper.warpExposure(
-            destWcs = coadd.getWcs(),
-            srcExposure = expo,
-            maxBBox = coadd.getBBox())
+    """
+    coadd = coadd_utils.Coadd.fromConfig(
+        bbox=dest_bbox,
+        wcs=dest_wcs,
+        config=coadd_config)
+    for j, expo in enumerate(expo_list):
+        warped_exposure = warper.warpExposure(
+            destWcs=coadd.getWcs(),
+            srcExposure=expo,
+            maxBBox=coadd.getBBox())
         log.info("warp{}".format(j))
-        #warpedExposure.writeFits("warp{}.fits".format(j))
         j += 1
-        coadd.addExposure(warpedExposure)
+        coadd.addExposure(warped_exposure)
 
     return coadd.getCoadd()
 
-def stitchExposuresGoodPixelCopy(destWcs, destBBox, expoList, warper,
-    badPixelMask = afwImage.MaskU.getPlaneBitMask(["EDGE"])):
-    ''' Return an exposure matching the destWcs and destBBox that is composed of
-    pixels from the exposures in expoList. Uses coadd_utils.goodPixelCopy
-    @ destWcs: WCS object for the destination exposure.
-    @ destBBox: Bounding box for the destination exposure.
-    @ expoList: List of exposures to combine to form dextination exposure.
+
+def stitch_exposures_good_pixel_copy(dest_wcs, dest_bbox, expo_list, warper,
+                                     bad_pixel_mask=None):
+    """ Return an exposure matching the dest_wcs and dest_bbox that is composed of
+    pixels from the exposures in expo_list. Uses coadd_utils.goodPixelCopy
+    @ dest_wcs: WCS object for the destination exposure.
+    @ dest_bbox: Bounding box for the destination exposure.
+    @ expo_list: List of exposures to combine to form dextination exposure.
     @ warper: Warper to use when warping images.
-    @ badPixelMask: mask for pixels that should not be copied.
+    @ bad_pixel_mask: mask for pixels that should not be copied.
     All exposures need valid WCS.
-    '''
-    destExpo = afwImage.ExposureF(destBBox, destWcs)
-    for j, expo in enumerate(expoList):
-        warpedExposure = warper.warpExposure(
-            destWcs = destExpo.getWcs(),
-            srcExposure = expo,
-            maxBBox = destExpo.getBBox())
-        srcMaskedImage = warpedExposure.getMaskedImage()
-        destMaskedImage = destExpo.getMaskedImage()
-        coaddUtils.copyGoodPixels(destMaskedImage, srcMaskedImage, badPixelMask)
-    return destExpo
+    """
+    if bad_pixel_mask is None:
+        bad_pixel_mask = afw_image.MaskU.getPlaneBitMask(["EDGE"])
+    dest_expo = afw_image.ExposureF(dest_bbox, dest_wcs)
+    for j, expo in enumerate(expo_list):
+        warped_exposure = warper.warpExposure(
+            destWcs=dest_expo.getWcs(),
+            srcExposure=expo,
+            maxBBox=dest_expo.getBBox())
+        src_masked_image = warped_exposure.getMaskedImage()
+        dest_masked_image = dest_expo.getMaskedImage()
+        coadd_utils.copyGoodPixels(dest_masked_image, src_masked_image, bad_pixel_mask)
+    return dest_expo
 
 
 def strExpoCornersRaDec(expo):
@@ -118,7 +116,7 @@ def strExpoCornersRaDec(expo):
     y0 = expo.getY0()
     w = expo.getWidth()
     h = expo.getHeight()
-    llCorner = wcs.pixelToSky(x0, y0)
-    urCorner = wcs.pixelToSky(x0+w, y0+h)
-    s = "llCorner={} urCorner={}".format(llCorner, urCorner)
+    ll_corner = wcs.pixelToSky(x0, y0)
+    ur_corner = wcs.pixelToSky(x0+w, y0+h)
+    s = "ll_corner={} ur_corner={}".format(ll_corner, ur_corner)
     return s
