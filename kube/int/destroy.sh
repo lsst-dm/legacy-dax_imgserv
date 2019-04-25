dd9901899190f0a5b0199df729ae7b8fb44bad04
Kenny Lo <kennylo@lsst-kub001.ncsa.illinois.edu>
2019-04-25 17:28:08 -0500

    Adjust to current way of accessing persistentvolumes

--- kube/int/destroy.sh
+++ kube/int/destroy.sh
@@ -4,5 +4,5 @@ DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-int'}
 kubectl delete ingress dax-imgserv-ingress --namespace $DAX_NAMESPACE
 kubectl delete service dax-imgserv-service --namespace $DAX_NAMESPACE
 kubectl delete deployment dax-imgserv-deployment --namespace $DAX_NAMESPACE
-kubectl delete pvc dax-imgserv-datasets-claim --namespace $DAX_NAMESPACE
-kubectl delete pv dax-int-imgserv-datasets-volume
+# kubectl delete pvc dax-imgserv-datasets-claim --namespace $DAX_NAMESPACE
+# kubectl delete pv dax-int-imgserv-datasets-volume
--- kube/stable/create.sh
+++ kube/stable/create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-stable'}
 
-kubectl create -f dax-imgserv-datasets-volume.yaml
-kubectl create -f dax-imgserv-datasets-claim.yaml --namespace $DAX_NAMESPACE
+# kubectl create -f dax-imgserv-datasets-volume.yaml
+# kubectl create -f dax-imgserv-datasets-claim.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-imgserv-deployment.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-imgserv-service.yaml --namespace $DAX_NAMESPACE
 kubectl create -f dax-imgserv-ingress.yaml --namespace $DAX_NAMESPACE
