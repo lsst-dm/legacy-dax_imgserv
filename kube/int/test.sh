a66f02183ffd5bf825adb4968db885ba7973327d
Kenny Lo <kennylo@slac.stanford.edu>
2019-04-25 17:28:08 -0500

    Adjust to current way of accessing persistentvolumes

--- kube/int/test.sh
+++ kube/int/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
 echo "ImgServe test at lsst-lsp-int ..."
-
-curl --fail -o /tmp/imgserv_int_circle.fits "https://lsst-lsp-int.ncsa.illinois.edu/api/image/soda/sync?ID=DC_W13_Stripe82.calexp.r&POS=CIRCLE+37.644598+0.104625+100"
+curl --fail -o /tmp/imgserv_int_circle.fits \
+-L "https://lsst-lsp-int.ncsa.illinois.edu/api/image/soda/sync?ID=DC_W13_Stripe82.calexp.r&POS=CIRCLE+37.644598+0.104625+100"
--- kube/stable/destroy.sh
+++ kube/stable/destroy.sh
@@ -4,5 +4,5 @@ DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-stable'}
 kubectl delete ingress dax-imgserv-ingress --namespace $DAX_NAMESPACE
 kubectl delete service dax-imgserv-service --namespace $DAX_NAMESPACE
 kubectl delete deployment dax-imgserv-deployment --namespace $DAX_NAMESPACE
-kubectl delete pvc dax-imgserv-datasets-claim --namespace $DAX_NAMESPACE
-kubectl delete pv dax-stable-imgserv-datasets-volume
+# kubectl delete pvc dax-imgserv-datasets-claim --namespace $DAX_NAMESPACE
+# kubectl delete pv dax-stable-imgserv-datasets-volume
