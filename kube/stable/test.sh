b7a4b7ce9c6d2ac5a87e330fd27df77a5ae98dfd
Kenny Lo <kennylo@slac.stanford.edu>
2019-08-20 15:57:26 -0700

    Update the smoke tests with correct value for CIRCLE radius

--- kube/stable/test.sh
+++ kube/stable/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -ex
 
-echo "ImgServ test at lsst-lsp-stable ..."
-
-curl --fail -o /tmp/imgserv_stable_circle.fits "https://lsst-lsp-stable.ncsa.illinois.edu/api/image/soda/sync?ID=DC_W13_Stripe82.calexp.r&POS=CIRCLE+37.644598+0.104625+100"
+echo "ImgServ smoke test at lsst-lsp-stable ..."
+curl --fail -o /tmp/imgserv_stable_circle.fits "https://lsst-lsp-stable.ncsa.illinois.edu/api/image/soda/sync?ID=DC_W13_Stripe82.calexp.r&POS=CIRCLE+37.644598+0.104625+0.027777777"
