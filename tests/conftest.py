65c6da7486cd052578139ebb358efd92bb4983d7
Kenny Lo <kennylo@slac.stanford.edu>
2020-10-05 14:32:00 -0700

    Fix pytest scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,7 @@ from flask import Flask
 from lsst.dax.imgserv import api_soda as is_api_soda
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
     app = Flask(__name__)
     app.register_blueprint(is_api_soda.image_soda, url_prefix='/api/image/soda')
