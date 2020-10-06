7504b56990855a5d03af70281e164e7458bf6391
Kenny Lo <kennylo@slac.stanford.edu>
2020-10-06 12:13:34 -0700

    Update smoke tests for LSP deployments

--- kube/int/test.sh
+++ kube/int/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 
 echo "ImgServe smoke test at lsst-lsp-int ..."
-curl --fail -o /tmp/imgserv_int_circle.fits -L "https://lsst-lsp-int.ncsa.illinois.edu/api/image/soda/sync?ID=DC_W13_Stripe82.calexp.r&POS=CIRCLE+37.644598+0.104625+0.027777777"
+curl --fail -o /tmp/imgserv_int_circle.fits -L \
+"https://lsst-lsp-int.ncsa.illinois.edu/api/image/soda/sync?ID=sdss_stripe82_01.calexp.r&POS=CIRCLE+37.644598+0.104625+0.027777777"
--- kube/stable/test.sh
+++ kube/stable/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 
 echo "ImgServ smoke test at lsst-lsp-stable ..."
-curl --fail -o /tmp/imgserv_stable_circle.fits "https://lsst-lsp-stable.ncsa.illinois.edu/api/image/soda/sync?ID=DC_W13_Stripe82.calexp.r&POS=CIRCLE+37.644598+0.104625+0.027777777"
+curl --fail -o /tmp/imgserv_stable_circle.fits \
+"https://lsst-lsp-stable.ncsa.illinois.edu/api/image/soda/sync?ID=sdss_stripe82_01.calexp.r&POS=CIRCLE+37.644598+0.104625+0.027777777"
