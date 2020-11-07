1005c3fe9116eccc16bef26524e0edd95c449df4
Kenny Lo <kennylo@slac.stanford.edu>
2020-11-06 20:04:07 -0600

    Configure container deployment for access to db secrets

--- kube/int/test.sh
+++ kube/int/test.sh
@@ -2,4 +2,4 @@
 
 echo "ImgServe smoke test at lsst-lsp-int ..."
 curl --fail -o /tmp/imgserv_int_circle.fits -L \
-"https://lsst-lsp-int.ncsa.illinois.edu/api/image/soda/sync?ID=sdss_stripe82_01.calexp.r&POS=CIRCLE+37.644598+0.104625+0.027777777"
+"https://lsst-lsp-int.ncsa.illinois.edu/api/image/soda/sync?ID=default.calexp.r&POS=CIRCLE+320.806258+-0.3313935+0.01"
