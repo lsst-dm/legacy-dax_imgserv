4f23036be9d67b3a56fe3ef04c6bfcb910394d11
Kenny Lo <kennylo@slac.stanford.edu>
2020-11-12 12:24:30 -0800

    Make naming of pv consistent with -int

--- kube/stable/create_secret.sh
+++ kube/stable/create_secret.sh
@@ -3,3 +3,5 @@ DAX_NAMESPACE=${DAX_NAMESPACE:-'lsst-lsp-stable-dax'}
 
 kubectl create secret generic dax-imgserv-config --from-file=./webserv.ini \
 --namespace $DAX_NAMESPACE
+kubectl create secret generic dax-db-auth --from-file=./webserv.ini \
+--namespace $DAX_NAMESPACE
--- kube/stable/test.sh
+++ kube/stable/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -ex
 
 echo "ImgServe smoke test at lsst-lsp-stable ..."
-curl --fail -o /tmp/lsp_stable_test.fits -L \
-"https://lsst-lsp-stable.ncsa.illinois.edu/api/image/soda/sync?ID=default.calexp.r&POS=CIRCLE+216.68+-0.53+0
-.01"
+curl -o /tmp/lsp_stable_test.fits -L \
+"https://lsst-lsp-stable.ncsa.illinois.edu/api/image/soda/sync?ID=default.calexp.r&POS=CIRCLE+216.68+-0.53+0.01"
