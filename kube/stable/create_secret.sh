de87644f56b1ce27075d64b72148a477230e0ffa
Kenny Lo <kennylo@slac.stanford.edu>
2020-11-12 12:27:17 -0800

    Correct the name for db-auth.yaml

--- kube/stable/create_secret.sh
+++ kube/stable/create_secret.sh
@@ -3,5 +3,5 @@ DAX_NAMESPACE=${DAX_NAMESPACE:-'lsst-lsp-stable-dax'}
 
 kubectl create secret generic dax-imgserv-config --from-file=./webserv.ini \
 --namespace $DAX_NAMESPACE
-kubectl create secret generic dax-db-auth --from-file=./webserv.ini \
+kubectl create secret generic dax-db-auth --from-file=./db-auth.yaml \
 --namespace $DAX_NAMESPACE
