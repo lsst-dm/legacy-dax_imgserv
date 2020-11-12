d3c89bfcde59668bb01a67699578f35a15c6e15e
Kenny Lo <kennylo@slac.stanford.edu>
2020-11-12 12:39:58 -0800

    Update file path for project directory in RSP

--- kube/int/create_secret.sh
+++ kube/int/create_secret.sh
@@ -3,3 +3,5 @@ DAX_NAMESPACE=${DAX_NAMESPACE:-'lsst-lsp-int-dax'}
 
 kubectl create secret generic dax-imgserv-config --from-file=./webserv.ini \
 --namespace $DAX_NAMESPACE
+kubectl create secret generic dax-db-auth --from-file=./db-auth.yaml \
+--namespace $DAX_NAMESPACE
